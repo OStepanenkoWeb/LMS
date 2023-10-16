1eee96fdb627bbb91163d6a61a5113306cdcf9e2
Stepanenko Oleg <o.stepanenko@id-mt.ru>
2023-10-16 15:16:35 +0300

    add refresh Token method

--- server/routes/route.user.ts
+++ server/routes/route.user.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { activateUser, loginUser, logoutUser, registrationUser } from '../controllers/user.controller'
+import { activateUser, loginUser, logoutUser, registrationUser, updateAccessToken } from '../controllers/user.controller'
 
 const userRouter = express.Router()
 
@@ -10,5 +10,7 @@ userRouter.post('/activate-user', activateUser)
 userRouter.post('/login', loginUser)
 
 userRouter.get('/logout', logoutUser)
+
+userRouter.get('/refresh-token', updateAccessToken)
 
 export default userRouter

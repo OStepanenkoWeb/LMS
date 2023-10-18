af17508a9a25b1e0a5ccab3667ada3910e9c3159
Stepanenko Oleg <o.stepanenko@id-mt.ru>
2023-10-18 16:37:02 +0300

    add services get course by id and courses list

--- server/controllers/cource.controller.ts
+++ server/controllers/cource.controller.ts
@@ -1,7 +1,7 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { CatchAsyncError } from '../middleware/catchAsyncError'
 import ErrorHandler from '../utils/errorHandler'
-import { createCourse, updateCourse } from '../services/cource.service'
+import { createCourse, getAllCoursesList, getCourseById, updateCourse } from '../services/cource.service'
 
 // upload course
 export const uploadCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
@@ -16,6 +16,24 @@ export const uploadCourse = CatchAsyncError(async (req: Request, res: Response, 
 export const editCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
   try {
     updateCourse(req, res, next)
+  } catch (error: any) {
+    next(new ErrorHandler(error.message, 400))
+  }
+})
+
+// get single course --- without purchasing
+export const getSingleCourse = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    getCourseById(req, res, next)
+  } catch (error: any) {
+    next(new ErrorHandler(error.message, 400))
+  }
+})
+
+// get all course --- without purchasing
+export const getAllCourses = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    getAllCoursesList(req, res, next)
   } catch (error: any) {
     next(new ErrorHandler(error.message, 400))
   }
--- server/services/cource.service.ts
+++ server/services/cource.service.ts
@@ -1,6 +1,7 @@
-import { type NextFunction, type Response, type Request } from 'express'
+import { type Response, type Request } from 'express'
 import { CatchAsyncError } from '../middleware/catchAsyncError'
 import CourseModel from '../models/course.model'
+import { redis } from '../utils/redis'
 
 // create course
 export const createCourse = CatchAsyncError(async (req: Request, res: Response) => {
@@ -28,5 +29,51 @@ export const updateCourse = CatchAsyncError(async (req: Request, res: Response) 
   res.status(201).json({
     success: true,
     course
+  })
+})
+
+// get course by id
+export const getCourseById = CatchAsyncError(async (req: Request, res: Response) => {
+  const courseId = req.params.id
+
+  const isCacheExist = await redis.get(courseId)
+
+  let course = null
+
+  if (isCacheExist) {
+    course = JSON.parse(isCacheExist)
+  } else {
+    course = await CourseModel
+      .findById(req.params.id)
+      .select('-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links')
+
+    await redis.set(courseId, JSON.stringify(course))
+  }
+
+  res.status(200).json({
+    success: true,
+    course
+  })
+})
+
+// get all courses
+export const getAllCoursesList = CatchAsyncError(async (req: Request, res: Response) => {
+  const isCacheExist = await redis.get('allCourses')
+
+  let courses = null
+
+  if (isCacheExist) {
+    courses = JSON.parse(isCacheExist)
+  } else {
+    courses = await CourseModel
+      .find()
+      .select('-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links')
+
+    await redis.set('allCourses', JSON.stringify(courses))
+  }
+
+  res.status(200).json({
+    success: true,
+    courses
   })
 })

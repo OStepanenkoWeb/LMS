56b42d9d6f90b8a323067c6c58d75b339f5e3e1f
Stepanenko Oleg <o.stepanenko@id-mt.ru>
2023-11-12 20:28:08 +0300

    fix upload files

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -9,9 +9,6 @@ import {
 } from '../controllers/user.controller'
 import { authorizeRoles, isAuthenticated } from '../middleware/auth'
 
-const multer = require('multer')
-const upload = multer({ dest: 'public/images' })
-
 const userRouter = express.Router()
 
 userRouter.post('/registration', registrationUser)
@@ -32,7 +29,7 @@ userRouter.put('/update-user-info', isAuthenticated, updateUserInfo)
 
 userRouter.put('/update-user-password', isAuthenticated, updatePassword)
 
-userRouter.put('/update-user-avatar', isAuthenticated, upload.single('image'), updateProfilePicture)
+userRouter.put('/update-user-avatar', isAuthenticated, updateProfilePicture)
 
 userRouter.get('/get-users', isAuthenticated, authorizeRoles('admin'), getAllUsers)
 
